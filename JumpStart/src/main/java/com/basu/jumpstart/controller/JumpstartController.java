4fc687e1e01f60e9d500fe424e9c4134877abbe4
sheshanathkumar <35036388+sheshanathkumar@users.noreply.github.com>
2020-09-12 00:33:58 +0530

    tried to add resolve cross origin but failed
    Handle cors origin from your side

--- JumpStart/src/main/java/com/basu/jumpstart/controller/JumpstartController.java
+++ JumpStart/src/main/java/com/basu/jumpstart/controller/JumpstartController.java
@@ -2,7 +2,9 @@ package com.basu.jumpstart.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,20 +13,31 @@ import com.basu.jumpstart.response.ModelViewResonse;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin( maxAge = 3600)
 public class JumpstartController {
 
 	
 	//@Autowired HttpRequest request;
 	
-	@GetMapping("/client-login")
+	
+	@GetMapping("/test")
+	public String getTest () {
+		return "Hello from JUMPSTART";
+	}
+	
+	@CrossOrigin("http://localhost:3000/")
+	@PostMapping("/client-login")
 	public ResponseEntity<ModelViewResonse> getClientLogin (@RequestParam("username") String username, @RequestParam("password") String password) {
 		
 		if (username != null && password != null) {
-		
 			try {
-				
-				
-				return new ResponseEntity<ModelViewResonse>(HttpStatus.OK);
+				if (username.equalsIgnoreCase("Shesho") && password.equalsIgnoreCase("12345")) {
+					
+					
+					return new ResponseEntity<ModelViewResonse>(new ModelViewResonse(""+HttpStatus.OK, "Login Success", "SUCCESS"), HttpStatus.OK);
+				} else {
+					return new ResponseEntity<ModelViewResonse>(HttpStatus.BAD_REQUEST);
+				}
 			}catch (Exception e) {
 				return new ResponseEntity<ModelViewResonse>(HttpStatus.BAD_REQUEST);
 			}
--- JumpStart/src/main/java/com/basu/jumpstart/response/ModelViewResonse.java
+++ JumpStart/src/main/java/com/basu/jumpstart/response/ModelViewResonse.java
@@ -1,8 +1,10 @@
 package com.basu.jumpstart.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class ModelViewResonse {
 
 	private String code;
